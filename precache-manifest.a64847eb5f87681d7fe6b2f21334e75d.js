d29e8fbe0bb78d5368e2d51d148a3f9e2cc5c43a
tkgka <ksh17171@gmail.com>
2022-01-12 11:04:11 +0000

    init

---
+++ precache-manifest.a64847eb5f87681d7fe6b2f21334e75d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c360f0fd66a2b8caec403fed811114",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "e2c10c375fb2bdc35ba4",
+    "url": "/css/app.5c775a73.css"
+  },
+  {
+    "revision": "159cb67fc3bc762a8c3232f0a0c6728e",
+    "url": "/fonts/VarelaRound-Regular.159cb67f.ttf"
+  },
+  {
+    "revision": "9d187cd2d0dccb9398f4b618f72801f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2c10c375fb2bdc35ba4",
+    "url": "/js/app.7ee6a228.js"
+  },
+  {
+    "revision": "0fc7e1244a12f1f63203",
+    "url": "/js/chunk-vendors.127a9ba0.js"
+  },
+  {
+    "revision": "7fc1d65ea370190d9486e3dcb2f903f6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
